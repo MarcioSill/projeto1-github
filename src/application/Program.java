84027ebf8789198ef48d706e4f0837987df1292d
MarcioSill <marcio.sill@hotmail.com>
2020-05-08 19:47:43 -0300

    Projeto atual

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,19 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite um numero:");
+		double val = sc.nextDouble();
+		
+		System.out.println("Val: " + val);
+		
+		
+		
+		sc.close();
 
 	}
 
